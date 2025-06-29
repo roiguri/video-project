210241cc04eb06cb0efef2460c213ab5974bc271
Itai Paperno <itaipaperno@mail.tau.ac.il>
2025-06-29 17:38:26 +0300

    Delete testing and reference

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Test package for the video processing project.
-
-This package contains all test modules for validating the
-video processing pipeline functionality.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Video Processing Team" 
